3889e20e1b2350b2f1509739aeb7c2e4cedcf3c0
Francisco <frodriguez25@ucol.mx>
2023-12-30 15:00:31 -0600

    Button component created

---
+++ src/Button.jsx
@@ -0,0 +1,27 @@
+// El primer paso será importar mediante npm el módulo de PropTypes (comando en google)
+// Después importamos como lo vemos abajo v
+import PropTypes from 'prop-types'
+
+// Recordemos que las props se importan entre llaves dentro de los argumentos.
+export function Button({text, name}) {
+    console.log(text)
+    // Validación de que haya texto
+    if(!text){
+        console.error("Text is required")        
+    }
+    return <button>
+        {text} - {name}
+    </button>
+}
+
+Button.propTypes = {
+    // Validación de texto usando propTypes
+    text: PropTypes.string.isRequired
+}
+
+Button.defaultProps = {
+    // Establecemos valores por defecto usando propTypes
+    // Sintaxis de objetos
+    name: 'Some user',
+    text: 'Some text'
+}
